50927c03633680b7521e49084caecebf31419f52
Tejendra86 <tejadodla8686@gmail.com>
2026-02-27 16:52:24 +0530

    UC2 palindrome

---
+++ App/src/UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        // Check if palindrome using if-else
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+    }
+}
